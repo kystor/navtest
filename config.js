628d84d329e9a9e60bf0030d8cc201a6641db51a
kystor <hcwinsjames@outlook.com>
2025-12-19 13:40:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,12 @@ module.exports = {
     username: process.env.ADMIN_USERNAME || 'admin',
     password: process.env.ADMIN_PASSWORD || '123456'
   },
+  app: {
+    title: process.env.SITE_TITLE || '我的导航' // 默认值为 '我的导航'
+  },
   server: {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || 'nav-item-jwt-secret-2024-secure-key'
   }
+
 }; 
